a629eea046c23d458635c6fdbb17a92278e3335e
David Paterson <dpaterson1@sheffield.ac.uk>
2019-07-12 13:45:37 +0100

    data and updates to scripts

--- commit_sentiment_analysis.py
+++ commit_sentiment_analysis.py
@@ -6,20 +6,20 @@ from pydriller import RepositoryMining
 from textblob import TextBlob
 from pathlib import Path
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 5:
     raise Exception("Usage: python3 commit_sentiment_analysis.py <working_dir> <commit_hash>")
 
-working_dir = sys.argv[1]
+project = sys.argv[1]
+working_dir = sys.argv[2]
+fix_commits = sys.argv[3].split(":")
+output_file=sys.argv[4]
 
-sentiments = {}
-
-for commit in RepositoryMining(working_dir).traverse_commits():
-    files_changed = [ x.filename for x in commit.modifications ]
-    blob = TextBlob(commit.msg)
-    sentiment = blob.sentences[0].polarity
-    for file in files_changed:
-        if not file in sentiments:
-            sentiments[file] = []
-        sentiments[file].extend([sentiment])
-
-print(sentiments)
+with open(output_file, 'w+') as outfile:
+    outfile.write("project,commit_hash,commit_msg,file_changed,sentiment_score,bug_fix_commit\n")
+    for commit in RepositoryMining(working_dir).traverse_commits():
+        files_changed = [ (x.new_path if x.new_path is not None else x.old_path) for x in commit.modifications ]
+        blob = TextBlob(commit.msg)
+        sentiment = blob.sentences[0].polarity
+        for file in files_changed:
+            outfile.write("{},{},{},{},{},{}\n".format(project,commit.hash,commit.msg.replace("\n","\\n").replace(",","."),file,sentiment,commit.hash in fix_commits))
+    outfile.close()
--- run_commit_sentiment_analysis.sh
+++ run_commit_sentiment_analysis.sh
@@ -5,22 +5,24 @@ die(){
   exit 1
 }
 
+trap SIGINT "die \"Caught signal CTRL+C. Shutting Down\""
 
 if [[ ! -d "tools/defects4j" ]]; then
   die "Defects4J not found inside tools folder. Did you run setup.sh?"
 fi
 D4J="tools/defects4j/framework/bin/defects4j"
 
-for project in Chart; do
+for project in Chart Lang Math Time Mockito Closure; do
   last_commit=$(tail -n 1 "tools/defects4j/framework/projects/$project/commit-db")
   max_bug_id=$(echo $last_commit | cut -d',' -f1)
   max_bug_commit_hash=$(echo $last_commit | cut -d',' -f3)
+  all_fix_commits=$(cut -d',' -f3 "tools/defects4j/framework/projects/$project/commit-db" | tr '\n' ':' | perl -p -e "s/(.*):$/\1/g")
   tmp_dir="/tmp/commitanalysis-$project-$max_bug_id-$$"
   mkdir -p $tmp_dir
   $D4J checkout -p $project -v ${max_bug_id}f -w $tmp_dir
   pushd $tmp_dir > /dev/null 2>&1
     git checkout -b sentiment_analysis $max_bug_commit_hash
   popd > /dev/null 2>&1
-  python3 commit_sentiment_analysis.py $tmp_dir
+  python3 commit_sentiment_analysis.py $project $tmp_dir $all_fix_commits "data/$project-sentiment-analysis.csv"
   rm -rf $tmp_dir
 done
