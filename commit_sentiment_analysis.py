01d00e0ea59a644d4c214d1aa80fc11f293eeed9
David Paterson <dpaterson1@sheffield.ac.uk>
2019-07-18 09:38:53 +0100

    latest data and changes to run script to omit uninterseting message information

--- commit_sentiment_analysis.py
+++ commit_sentiment_analysis.py
@@ -2,6 +2,7 @@ import tempfile
 import os
 import subprocess
 import sys
+import re
 from pydriller import RepositoryMining
 from textblob import TextBlob
 from pathlib import Path
@@ -13,13 +14,21 @@ project = sys.argv[1]
 working_dir = sys.argv[2]
 fix_commits = sys.argv[3].split(":")
 output_file=sys.argv[4]
-
+line_rex = re.compile(".*\n")
 with open(output_file, 'w+') as outfile:
-    outfile.write("project,commit_hash,commit_msg,file_changed,sentiment_score,bug_fix_commit\n")
+    outfile.write("project,commit_hash,commit_msg,file_changed,sentiment_score,subjectivity_score,bug_fix_commit\n")
     for commit in RepositoryMining(working_dir).traverse_commits():
         files_changed = [ (x.new_path if x.new_path is not None else x.old_path) for x in commit.modifications ]
-        blob = TextBlob(commit.msg)
-        sentiment = blob.sentences[0].polarity
+        commit_filter = ""
+        for line in line_rex.findall(commit.msg):
+            if line.strip() != '' and not line.startswith("git-svn-id"):
+                if commit_filter != '':
+                    commit_filter = commit_filter + "\n"
+                commit_filter = commit_filter + line.strip()
+
+        blob = TextBlob(commit_filter)
+        sentiment = blob.sentiment.polarity
+        subjectivity = blob.sentiment.subjectivity
         for file in files_changed:
-            outfile.write("{},{},{},{},{},{}\n".format(project,commit.hash,commit.msg.replace("\n","\\n").replace(",","."),file,sentiment,commit.hash in fix_commits))
+            outfile.write("{},{},{},{},{},{},{}\n".format(project,commit.hash,commit_filter.replace("\n","\\n").replace(",",".").replace("\"","\\\""),file,sentiment,subjectivity,commit.hash in fix_commits))
     outfile.close()
